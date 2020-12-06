4919218474793dabf3ecb669158d347777e243d8
Ebert Soares <59069184+Ebert-Soares@users.noreply.github.com>
2020-12-06 19:37:52 -0300

    Testes com char

---
+++ Testes com char.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+main(){
+char ch;  
+	
+printf("Teste: \n");
+scanf("%c", &ch);
+
+printf("Inteiro:%c \n", ch);
+printf("Inteiro seguinte: %c\nNumero ASCII: %d", ch + 1, (int) ch);
+		
+}
